eca9e68362676f14417495f36a0f692dd2af18da
Vivek <vivek.kumar@uolo.com>
2024-03-31 15:45:49 +0530

    context for gloabl language, added banner, and Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import CoinPage from './pages/CoinPage';
+import { makeStyles } from '@material-ui/core';
+import Homepage from './pages/HomePage';
+
 function App() {
-  return <div className='App'></div>;
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: '#14161a',
+      color: 'white',
+      minHeight: '100vh',
+    },
+  }));
+  const classes = useStyles();
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Homepage />} />
+          <Route path='/coins/:id' element={<CoinPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { makeStyles, Container, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  banner: {
+    backgroundImage: 'url(./images/banner2.jpg)',
+  },
+  bannerContent: {
+    height: 400,
+    display: 'flex',
+    flexDirection: 'column',
+    paddingTop: 25,
+    justifyContent: 'space-around',
+  },
+  tagline: {
+    display: 'flex',
+    height: '40%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+}));
+
+const Banner = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant='h2'
+            style={{
+              fontWeight: 'bold',
+              marginBottom: 15,
+              fontFamily: 'Montserrat',
+            }}
+          >
+            My First BTC
+          </Typography>
+          <Typography
+            variant='subtitle2'
+            style={{
+              color: 'darkgrey',
+              textTransform: 'capitalize',
+              fontFamily: 'Montserrat',
+            }}
+          >
+            All your favorite Crypto Currency at one place
+          </Typography>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
